a588fa780f62153c83ce3b65556f74af76382989
henryqi-goldenbear <136924834+henryqi-goldenbear@users.noreply.github.com>
2026-04-19 13:55:33 -0700

    fix: convert record to @Data class for Java 11 (4/4 MatchStatusUpdateRequest)

--- src/main/java/com/henryqi/voicematching/dto/MatchStatusUpdateRequest.java
+++ src/main/java/com/henryqi/voicematching/dto/MatchStatusUpdateRequest.java
@@ -1,3 +1,12 @@
 package com.henryqi.voicematching.dto;
 
-public record MatchStatusUpdateRequest(String status) {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MatchStatusUpdateRequest {
+    private String status;
+}
