fdc4bc896b0ba903ece515cb0a459e049510e4d7
henryqi-goldenbear <136924834+henryqi-goldenbear@users.noreply.github.com>
2026-04-19 13:54:07 -0700

    fix: convert record DTOs to @Data classes for Java 11 (1/4 AiExtraction)

--- src/main/java/com/henryqi/voicematching/dto/AiExtraction.java
+++ src/main/java/com/henryqi/voicematching/dto/AiExtraction.java
@@ -1,7 +1,14 @@
 package com.henryqi.voicematching.dto;
 
-public record AiExtraction(
-        String emotionalState,
-        String lifeChapter,
-        String socialEnergy
-) {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AiExtraction {
+    private String emotionalState;
+    private String lifeChapter;
+    private String socialEnergy;
+}
